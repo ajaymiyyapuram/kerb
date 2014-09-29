25f46ec5baa70e0faab27c7b4fec4688a01d1dda
Ajay <ajay.miyyapuram@gmail.com>
2014-09-29 00:56:48 -0500

    refactored code

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,13 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2VyYiIsImEiOiJpdXNnU0V3In0.EzvrG2coRiHt9Up-TpeRuw#18/45.67922/-111.03717';
 // var namespace = 'kerb.h5cpkg76';
 
-$(document).ready(function() {
+$(document).ready(function () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -16,103 +10,94 @@ $(document).ready(function() {
 });
 
 // callback for getcurrentposition
-function showPosition(position) {    
-    var nearByNotifications = getNearByNotifications(position.coords.latitude, position.coords.longitude);
+function showPosition(position) {
+    var map = L.mapbox.map('map', 'examples.map-20v6611k');
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    map.setView([latitude, longitude], 15);
+
+    var usersLayer = createLayerData(JSON.parse(
+            '{ "notifications" : [' +
+            '{ "latitude":"' + latitude + '", "longitude":"' + longitude + '", "name":"User" } ]}'
+            ));
+
+    plotLocations(usersLayer, map);
+    animate(usersLayer, map);
+
     // plotLocation(user);
     // animate(radius, updateinterval, layer, map);
+
+    var nearByNotifications = getNearByNotifications(latitude, longitude);
+    var notificationsLayer = createLayerData(nearByNotifications);
+
     // plotLocation(notifications)
     // animate(radius, updateinterval, layer, map);
-    plotPositions(position.coords.latitude, position.coords.longitude, nearByNotifications);
+
+    plotLocations(notificationsLayer, map);
+    animate(notificationsLayer, map);
+
+
+    // plotPositions(position.coords.latitude, position.coords.longitude, nearByNotifications);
+}
+
+function createLayerData(data) {
+    var features = [];
+
+    // Add all nearby notifications to feature list.
+    for (var i = 0; i < data.notifications.length; i++) {
+        features.push({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [data.notifications[i].longitude, data.notifications[i].latitude]
+            },
+            properties: {
+                'marker-color': '#000',
+                'marker-symbol': 'circle',
+                title: data.notifications[i].name,
+                count: 20
+            }
+        });
+    }
+
+    var layer = {
+        type: 'FeatureCollection',
+        features: features
+    };
+
+    return layer;
 }
 
 function getNearByNotifications(userlat, userlong) {
-    
+
     // use jqery ajax to call web service
     // send user latlong, and fetch notifcations with in certain radius
-    
+
     /*
-    $.ajax({
-        url: "http://awsserver/webservice/getnearbynotifications.json?lat=userlat&long=userlong&rad=radius",
-        type: "GET",
-        cache: true,
-        dataType: 'jsonp',
-        error: function(jqXHR, textStatus, errorThrown) {
-            // alert("failure-"+jqXHR.statusText+"-,"+jqXHR.responseText+","+errorThrown.name+","+errorThrown.stack+","+errorThrown.Message+","+textStatus);
-        },
-        success: function(data, statusText) {
-          if(data.length > 0) {
-            return data;
-          } else {	
-            // some error occurred
-          }
-        }
-    }); */
-            
+     $.ajax({
+     url: "http://awsserver/webservice/getnearbynotifications.json?lat=userlat&long=userlong&rad=radius",
+     type: "GET",
+     cache: true,
+     dataType: 'jsonp',
+     error: function(jqXHR, textStatus, errorThrown) {
+     // alert("failure-"+jqXHR.statusText+"-,"+jqXHR.responseText+","+errorThrown.name+","+errorThrown.stack+","+errorThrown.Message+","+textStatus);
+     },
+     success: function(data, statusText) {
+     if(data.length > 0) {
+     return data;
+     } else {	
+     // some error occurred
+     }
+     }
+     }); */
+
     // hard coding the notifications for demo
     var notifications = '{ "notifications" : [' +
             '{ "latitude":"38.878832", "longitude":"-94.664753", "name":"Fitness Center" },' +
             '{ "latitude":"38.879618", "longitude":"-94.667264", "name":"Buffalo Wild Wings" },' +
             '{ "latitude":"38.873451", "longitude":"-94.666906", "name":"Face Fancies" } ]}';
 
-    return JSON.parse(notifications);    
-}
-
-function plotPositions(latitude, longitude, nearByNotifications) {
-
-    var map = L.mapbox.map('map', 'examples.map-20v6611k'); 
-    var features_Notifications = [];
-
-    // Add all nearby notifications to feature list.
-    for (var i = 0; i < nearByNotifications.notifications.length; i++) {
-        features_Notifications.push({
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [nearByNotifications.notifications[i].longitude, nearByNotifications.notifications[i].latitude]
-            },
-            properties: {
-                'marker-color': '#000',
-                'marker-symbol': 'circle',
-                title: nearByNotifications.notifications[i].name,
-                count: 20
-            }
-        });
-    }
-
-    var notificationsLayer = {
-        type: 'FeatureCollection',
-        features: features_Notifications
-    };
-
-    var features_Users = [];
-    
-    // Add user location to feature list.
-    features_Users.push({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [longitude, latitude]
-        },
-        properties: {
-            'marker-color': '#000',
-            'marker-symbol': 'circle',
-            title: 'User',
-            count: 20
-        }
-    });
-
-    var usersLayer = {
-        type: 'FeatureCollection',
-        features: features_Users
-    };
-
-    map.setView([latitude, longitude], 15);
-
-    plotLocationsAsCircleMarker(usersLayer, map);
-    animateCircleMarker(usersLayer, map);
-
-    plotLocationsAsCircleMarker(notificationsLayer, map);
-    animateCircleMarker(notificationsLayer, map);
+    return JSON.parse(notifications);
 }
 
 function cycleThroughNotifications(markers) {
@@ -126,9 +111,9 @@ function cycleThroughNotifications(markers) {
     run();
 }
 
-function plotLocationsAsCircleMarker(layer, map) {
+function plotLocations(layer, map) {
     L.geoJson(layer, {
-        pointToLayer: function(feature, latlng) {
+        pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, {
                 radius: 5
             })
@@ -136,7 +121,7 @@ function plotLocationsAsCircleMarker(layer, map) {
     }).addTo(map);
 }
 
-function animateCircleMarker(layer, map) {
+function animate(layer, map) {
     var circleRadius = 6;
     var reachedMaxRadius = false;
     var geoJson;
@@ -146,7 +131,7 @@ function animateCircleMarker(layer, map) {
 
         // plot circle marker
         geoJson = L.geoJson(layer, {
-            pointToLayer: function(feature, latlng) {
+            pointToLayer: function (feature, latlng) {
                 return L.circleMarker(latlng, {
                     radius: circleRadius,
                     color: "red"
@@ -157,13 +142,13 @@ function animateCircleMarker(layer, map) {
         geoJson.addTo(map);
 
         // update circle radius and redraw
-        window.setTimeout(function() {
+        window.setTimeout(function () {
             map.removeLayer(geoJson);
             if (reachedMaxRadius === false)
                 circleRadius = circleRadius + 1;
             else
                 circleRadius = circleRadius - 1;
-            
+
             if (circleRadius > 50) {
                 circleSizeUpdateInterval = 120;
                 reachedMaxRadius = true;
@@ -172,7 +157,7 @@ function animateCircleMarker(layer, map) {
                 circleSizeUpdateInterval = 10;
                 reachedMaxRadius = false;
             }
-            
+
             run();
         }, circleSizeUpdateInterval);
     }
