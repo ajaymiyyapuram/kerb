3268516947dd04ff53219811a4832bdc918fc100
Ajay <ajay.miyyapuram@gmail.com>
2014-10-01 00:09:44 -0500

    refactored code

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,10 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoia2VyYiIsImEiOiJpdXNnU0V3In0.EzvrG2coRiHt9Up-TpeRuw#18/45.67922/-111.03717';
+//L.mapbox.accessToken = 'pk.eyJ1Ijoia2VyYiIsImEiOiJpdXNnU0V3In0.EzvrG2coRiHt9Up-TpeRuw#18/45.67922/-111.03717';
+L.mapbox.accessToken = 'pk.eyJ1Ijoia2VyYiIsImEiOiJpdXNnU0V3In0.EzvrG2coRiHt9Up-TpeRuw#15/38.878832/-94.664753';
+// var map = L.mapbox.map('map','kerb.h5cpkg76');
+
 // var namespace = 'kerb.h5cpkg76';
+var userMarkerColor = 'black';
+var notificationMarkerColor = 'red';
 
 $(document).ready(function () {
     if (navigator.geolocation) {
@@ -11,27 +16,28 @@ $(document).ready(function () {
 
 // callback for getcurrentposition
 function showPosition(position) {
-    
+
     var map = L.mapbox.map('map', 'examples.map-20v6611k');
-    
+    // var map = L.mapbox.map('map', 'kerb.h5cpkg76');
+
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    map.setView([latitude, longitude], 12);
+    map.setView([latitude, longitude], 16);
 
     var usersLayer = createLayerData(JSON.parse(
             '{ "notifications" : [' +
             '{ "latitude":"' + latitude + '", "longitude":"' + longitude + '", "name":"User" } ]}'
-            ), '#000', 'circle', 20);
-    plotLocations(usersLayer, map);
-    animate(usersLayer, map, 6, 10, 30, 10, false, "black"); 
+            ), '#000', 'circle', 20, 'black');
+    plotLocations(usersLayer, map, 15);
+    animate(usersLayer, map, 6, 10, 30, 10, false, userMarkerColor);
     // animate(usersLayer, map);
 
     var nearByNotifications = getNearByNotifications(latitude, longitude);
     var notificationsLayer = createLayerData(nearByNotifications, '#000', 'circle', 20);
-    plotLocations(notificationsLayer, map);
-    animate(notificationsLayer, map, 6, 10, 60, 10, true, "red"); 
+    plotLocations(notificationsLayer, map, 5, 'green');
+    animate(notificationsLayer, map, 6, 10, 60, 10, true, notificationMarkerColor);
     // cycleThroughNotifications(notificationMarkers);
-}   
+}
 
 function createLayerData(data, markercolor, markersymbol, markersize) {
     var features = [];
@@ -89,7 +95,8 @@ function getNearByNotifications(userlat, userlong) {
             '{ "latitude":"38.878832", "longitude":"-94.664753", "name":"Fitness Center" },' +
             '{ "latitude":"38.879618", "longitude":"-94.667264", "name":"Buffalo Wild Wings" },' +
             '{ "latitude":"38.873451", "longitude":"-94.666906", "name":"Face Fancies" } ]}';
-
+// 45.67922/-111.03717
+// 38.878832/-94.664753
     return JSON.parse(notifications);
 }
 
@@ -104,12 +111,13 @@ function cycleThroughNotifications(markers) {
     run();
 }
 
-function plotLocations(layer, map) {
+function plotLocations(layer, map, radius, color) {
     L.geoJson(layer, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, {
-                radius: 5
-            })
+                radius: radius,
+                color: color
+            });
         }
     }).addTo(map);
 }
@@ -143,8 +151,8 @@ function animate(layer, map, circleinitrad, circleminrad, circlemaxrad, updateIn
                 circleRadius = circleRadius - 1;
 
             if (circleRadius > circlemaxrad) {
-                
-                if(circleinout === true) {
+
+                if (circleinout === true) {
                     circleSizeUpdateInterval = 120;
                     reachedMaxRadius = true;
                 } else {
